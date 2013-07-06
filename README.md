bb0c753a514e17a7dfd1a98db9b54d04179ed05b
Angel Avgerinos <avgerinos@alfred>
2013-07-06 14:36:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Boostraping my Debian 7 Wheezy
+
+This is an opinionated script that boostraps my Debian 7 wheezy
+installation.
+
+Some configurations are borrowed from @agorf
+
+Nothing fancy here
+
+## Packages
+
+It installs and configures (according to my taste) the following
+packages
+
+* vim (Janus)
+* i3
+* Ruby (1.9.3-p327 with falcon patch)
+* Rails (3.2.13)
+* htop
+* gcc
+* build-essential
+* rxvt-unicode-256color
+* git
+
+Feel free to fork!
---
+++ bootstrap.sh
@@ -0,0 +1,42 @@
+echo "Bootstraping Debian 7 workstation"
+echo "The process will need your sudo password"
+
+echo "Updating repositories"
+apt-get -y update
+echo "Updating Packages"
+apt-get upgrade
+apt-get dist-upgrade
+
+# Install Required Packages
+echo "Installing sexy stuff"
+apt-get install curl build-essential gcc vim i3 rxvt-unicode-256color git htop
+
+# Install RVM
+echo "Installing RVM"
+\curl -L https://get.rvm.io | bash
+
+# Install Ruby 1.9.3-p327 with falcon patch
+echo "Installing Ruby 1.9.3-p327 with falcon patch"
+rvm install 1.9.3-p327 --patch falcon --force-autoconf -j 5
+rvm use 1.9.3-p327 --default
+
+# Installing Rails 3.2.13
+echo "Installing Rails 3.2.13"
+gem install rails --version 3.2.13 --no-ri --no-rdoc
+
+# Configuring VIM
+echo "Configuring vim"
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+  curl -Sso ~/.vim/autoload/pathogen.vim \
+      https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+mv /home/`whoami`/.vim/janus/vim/vimrc /home/`whoami`/vimrc.oldz
+cp /home/`whoami`/bootstrap/conf/vimrc /home/`whoami`/.vim/janus/vim/vimrc
+
+# Configuring i3
+echo "Configuring i3"
+cp /home/`whoami`/bootstrap/conf/i3/config /home/`whoami`/.i3/config
+cp /home/`whoami`/bootstrap/conf/i3/startup.sh /home/`whoami`/.i3/startup.sh
+
+echo "Adding Xdefaults"
+cp /home/`whoami`/bootstrap/conf/Xdefaults /home/`whoami`/.Xdefaults
