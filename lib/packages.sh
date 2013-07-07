d604e368c63ef86d5466becf1476053144fb8a4d
Angel Avgerinos <evaggelos.avgerinos@gmail.com>
2013-07-07 18:39:38 +0300

    Add sudo to packages

--- lib/packages.sh
+++ lib/packages.sh
@@ -1,8 +1,8 @@
 echo "Updating repositories"
-apt-get -y update
+sudo apt-get -y update
 echo "Updating Packages"
-apt-get dist-upgrade
+sudo apt-get dist-upgrade
 
 # Install Required Packages
 echo "Installing sexy stuff"
-apt-get install curl build-essential gcc vim i3 rxvt-unicode-256color git htop
+sudo apt-get install curl build-essential gcc vim i3 rxvt-unicode-256color git htop
